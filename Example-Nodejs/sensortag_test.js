c866f94c3942d49bf4d9b3003af87b60a3d2aa53
IOT2016 <fel.hoehn@googlemail.com>
2016-07-07 14:39:03 +0200

    Add files via upload

---
+++ Example-Nodejs/sensortag_test.js
@@ -0,0 +1,67 @@
+var util = require('util');
+
+var async = require('async');
+
+var SensorTag = require('./index');
+var httpsPost = require ('./lib/https_request')
+
+var USE_READ = true;
+
+SensorTag.discover(function(sensorTag) {
+  console.log('discovered: ' + sensorTag);
+
+  sensorTag.on('disconnect', function() {
+    console.log('disconnected!');
+    process.exit(0);
+  });
+
+  async.series([
+      function(callback) {
+        console.log('connectAndSetUp');
+        sensorTag.connectAndSetUp(callback);
+      },
+      function(callback) {
+        console.log('enableIrTemperature');
+        sensorTag.enableIrTemperature(callback);
+      },
+      function(callback) {
+        setTimeout(callback, 2000);
+      },
+      function(callback) {
+        if (USE_READ) {
+          console.log('readIrTemperature');
+          sensorTag.readIrTemperature(function(error, objectTemperature, ambientTemperature) {
+            console.log('\tobject temperature = %d °C', objectTemperature.toFixed(1));
+            console.log('\tambient temperature = %d °C', ambientTemperature.toFixed(1));
+			httpsPost.tmp(ambientTemperature.toFixed(2));
+			setTimeout(callback, 5000);
+          });
+        } else {
+          sensorTag.on('irTemperatureChange', function(objectTemperature, ambientTemperature) {
+            console.log('\tobject temperature = %d °C', objectTemperature.toFixed(1));
+            console.log('\tambient temperature = %d °C', ambientTemperature.toFixed(1))
+          });
+          console.log('setIrTemperaturePeriod');
+          sensorTag.setIrTemperaturePeriod(500, function(error) {
+            console.log('notifyIrTemperature');
+            sensorTag.notifyIrTemperature(function(error) {
+              setTimeout(function() {
+                console.log('unnotifyIrTemperature');
+                sensorTag.unnotifyIrTemperature(callback);
+              }, 5000);
+            });
+          });
+        }
+      },
+      function(callback) {
+        console.log('disableIrTemperature');
+        sensorTag.disableIrTemperature(callback);
+	  },
+      function(callback) {
+        console.log('disconnect');
+        sensorTag.disconnect(callback);
+      }
+   
+   ]
+  );
+});
